041c73c3d76460bc96e04b426fa33e19ab66697f
arneauvdm <arneauvdm@icetech.io>
2025-10-24 10:54:27 +0200

    gpt2_play.ipynb download and investigate the weights from huggingface GPT2 module that has been translated from tensorflow to pytorch train_gpt2.py is the module for building GPT2 from scratch currently 3 classes: -  GPT = configuration of full GPT module - Block = class that encodes entire transformer block, contains the attention blocks and MLP - MLP = simple feed forward neural network that is fully connected

---
+++ src/train_gpt2.py
@@ -0,0 +1,53 @@
+import torch.nn as nn
+from dataclasses import dataclass
+import math
+import torch
+# ================================================================================================
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layers: int = 6
+    n_heads: int = 6
+    n_embd: int = 384
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd,4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4* config.n_embd, config.n_embd)
+
+    def feed_forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CasualSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def feed_forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wtp = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layers)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
